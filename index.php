649f955a70c15fb7bf11c7505cca01627bd80ced
1509184 <fm.oyindamola@gmail.com>
2016-03-07 10:49:21 +0000

    dfgfhgsdf

---
+++ index.php
@@ -0,0 +1,19 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 1509184
+ * Date: 07/03/2016
+ * Time: 10:47
+ */<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+   <?php
+      echo "Hello World";
+   ?>
+</p>
+</body>
+<
+/html>
