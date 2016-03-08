bf8274a9cf7af61c5a402174f98e6fe02e5d3e63
1509184 <fm.oyindamola@gmail.com>
2016-03-08 10:07:49 +0000

    dfgfhgsdf

--- index.php
+++ index.php
@@ -5,13 +5,19 @@
 <body>
 <p>
    <?php
-      $myName = "Malick";
-      if($myName== "malick"){
-         print "I know You";
+      $myAge=20;
+      if ($myAge>16) {
+          print "Buy Specs";
       }
-      else{
-          print "who are you?";
-   }
+      elseif($myAge>18) {
+          print "Buy Mugs";
+      }
+      elseif($myAge>21){
+          print "Buy Sausage Rolls";
+      }
+      else {
+          print "You are a Minor";
+      }
    ?>
 </p>
 </body>
