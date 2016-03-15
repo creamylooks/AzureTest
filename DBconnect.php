6add094fd02b8b415a3308bc2947593a660fbd98
1509184 <fm.oyindamola@gmail.com>
2016-03-15 09:54:48 +0000

    dfgfhgsdf

---
+++ DBconnect.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1509184
+ * Date: 14/03/2016
+ * Time: 09:44
+ */
+define('DB_SERVER','creamy');
+define('DB_USERNAME','b28b1ba3c7f01f');
+define('DB_PASSWORD','12D5F6D8');
+define('DB_DATABASE','database');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
