a59730b79f597bae70326a179339d402e10277f4
1509184 <fm.oyindamola@gmail.com>
2016-03-15 10:40:32 +0000

    dfgfhgsdf

--- week7/DBconnect.php
+++ week7/DBconnect.php
@@ -7,7 +7,7 @@
  */
 define('DB_SERVER','ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME','b28b1ba3c7f01f');
-define('DB_PASSWORD','12D5F6D8');
+define('DB_PASSWORD','12d5f6d8');
 define('DB_DATABASE','creamy');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
