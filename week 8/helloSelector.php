4617afde5a843afb785ffc782159574b8dfd56c3
1509184 <fm.oyindamola@gmail.com>
2016-03-21 10:50:32 +0000

    dfgfhgsdf

---
+++ week 8/helloSelector.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Hello</title>
+</head>
+<body>
+<p>Where would you like to say hello?</p>
+<a href="helloPrinter.php">Earth</a>
+<a href="helloPrinter.php">Mars</a>
+<a href="helloPrinter.php">Uranus</a>
+
+</body>
