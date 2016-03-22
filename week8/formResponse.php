b3dbcd23a33b5d797b16a592a38b9951dcf864a0
1509184 <fm.oyindamola@gmail.com>
2016-03-22 10:17:12 +0000

    dfgfhgsdf

---
+++ week8/formResponse.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1509184
+ * Date: 22/03/2016
+ * Time: 10:14
+ */
+$forename=$_POST["forename"];
+$surname=$_POST["surname"];
+echo "$forename.". "  $surname";
