0e7662f51b55698d469e30db454f56cd8f33b00a
1509184 <fm.oyindamola@gmail.com>
2016-03-22 10:19:07 +0000

    dfgfhgsdf

---
+++ week8/form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>form</title>
+</head>
+<body>
+<form action="formResponse.php" method="post">
+    <label>Forename</label><input type="text" name="forename">
+    <label>Surname</label><input type="text" name="surname">
+    <input type="submit" value="submit">
+    </form>
+</body>
