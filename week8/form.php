2c6c2081fb27b30ff707a026105f1574e34d92a2
1509184 <fm.oyindamola@gmail.com>
2016-03-22 10:21:08 +0000

    dfgfhgsdf

--- week8/form.php
+++ week8/form.php
@@ -6,8 +6,8 @@
 </head>
 <body>
 <form action="formResponse.php" method="post">
-    <label>Forename</label><input type="text" name="forename">
-    <label>Surname</label><input type="text" name="surname">
+    <label>Forename</label><input type="text" name="forename"> <br>
+    <label>Surname</label><input type="text" name="surname"><br>
     <input type="submit" value="submit">
     </form>
 </body>
