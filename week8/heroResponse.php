8afa8c582ae9c78b4a8bab8ce7d4368ccd231869
1509184 <fm.oyindamola@gmail.com>
2016-03-22 10:46:35 +0000

    dfgfhgsdf

---
+++ week8/heroResponse.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1509184
+ * Date: 22/03/2016
+ * Time: 10:41
+ */
+$firstname=$_POST["firstname"];
+$surname=$_POST["surname"];
+$gender=$_POST["gender"];
+$superhero=$_POST["superhero"];
