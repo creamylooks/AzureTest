a57e0047aa1b887a9a13d35bd91207344667cbc6
1509184 <fm.oyindamola@gmail.com>
2016-03-22 10:47:08 +0000

    dfgfhgsdf

---
+++ week8/superHero.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>superHero</title>
+</head>
+<body>
+<form action="heroResponse.php" method="post">
+    <label>Firstname:</label><br>
+    <input type="text" name="firstname" placeholder="Firstname"><br>
+    <label>Surname:</label><br>
+    <input type="text" name="surname" placeholder="surname"><br>
+    <input type="checkbox" name="gender" value="male">
+    <input type="checkbox" name="gender" value="female"> <br>
+    <label>Superhero</label>
+    <input type="text" name="superhero" placeholder=" your superhero"><br>
+    <input type="submit" value="submit">
+</body>
