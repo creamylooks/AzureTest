bbb60bcbc8059ab2a68a42131c84b0a2971c57be
1509184 <fm.oyindamola@gmail.com>
2016-04-11 10:40:06 +0100

    WEEK9

--- week9/checklogin.php
+++ week9/checklogin.php
@@ -5,8 +5,10 @@
  * Date: 11/04/2016
  * Time: 09:48
  */
-$username=$_POST['username'];
-$password=$_POST['password'];
-if ($username=='damola' && $password=='damola'){
+
+$username=$_POST["username"];
+$password=$_POST["password"];
+if ($username=="damola" && $password=="damola"){
   setcookie('access_level','standarduser');
 }
+header('location:homepage.php');
--- week9/homepage.php
+++ week9/homepage.php
@@ -5,11 +5,15 @@
  * Date: 11/04/2016
  * Time: 09:55
  */
-function displayAccessLevelInformation($accessLevel){
-    if (access_level == "standarduser"){
+session_start();
+$accessLevel = $_COOKIE['access_level'];
+displayAccessLevelInformation($accessLevel);
+function displayAccessLevelInformation($accessLevel)
+{
+    if ($accessLevel == "standarduser"){
         echo "<p> You are currently logged in as a standard user </p>";
     }
-    elseif(access_level=="root"){
+    elseif($accessLevel=="root"){
         echo "<p> You are currently logged in as a root user </p>";
         echo "<p> You now have access to additional administrative features</p>";
     }
