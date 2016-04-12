753886021cde8ac59d01d376e453713003a48f3b
1509184 <fm.oyindamola@gmail.com>
2016-04-12 09:47:58 +0100

    week9b

---
+++ week9/printGET.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1509184
+ * Date: 12/04/2016
+ * Time: 09:44
+ */
+$username=$_GET{"username"};
+$password=$_GET{"password"};
+echo $username.$password;
