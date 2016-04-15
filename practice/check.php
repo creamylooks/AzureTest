3771f8689410a5c63f774bb7d457a62a8d7f3d66
1509184 <fm.oyindamola@gmail.com>
2016-04-15 20:19:27 +0100

    week9b

--- practice/check.php
+++ practice/check.php
@@ -9,15 +9,8 @@
 <?php
 for ($a=1; $a<=30; $a++)
 {
-   if ($a = 1) {
-        echo "on the " . $a . "st  of the month specs are available";
-   }
-    if ($a % 2 == 0) {
-        if ($a = 2 || $a = 22) {
-            echo "on the " . $a . "nd  of the month specs are available";
-        } else {
-            echo "on the " . $a . "th of the month specs are available";
-        }
+    if ($a % 4 == 0) {
+        echo "on the " . $a . "th of the month specs are available";
     }
     if ($a % 3 == 0) {
         if ($a = 3) {
@@ -29,12 +22,22 @@ for ($a=1; $a<=30; $a++)
         }
 
     }
-    if ($a % 4 == 0) {
-        echo "on the " . $a . "th of the month specs are available";
-    }
-    if($a %2 !=0 && $a%3 !=0 && $a%4 !=0){
-        echo "nothing is available on the ".$a."th of the month";
+    if ($a % 2 == 0) {
+        if ($a = 2 || $a = 22) {
+            echo "on the " . $a . "nd  of the month specs are available";
+        } else {
+            echo "on the " . $a . "th of the month specs are available";
+        }
+   if ($a = 1) {
+        echo "on the " . $a . "st  of the month specs are available";
    }
+
+    }
+
+
+   // if($a %2 !=0 && $a%3 !=0 && $a%4 !=0){
+     //   echo "nothing is available on the ".$a."th of the month";
+   //}
 
 
 
