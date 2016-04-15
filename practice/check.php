fc8376d5c9cc8c471f068b43394ae99046217fb1
1509184 <fm.oyindamola@gmail.com>
2016-04-15 19:32:53 +0100

    week9b

--- practice/check.php
+++ practice/check.php
@@ -6,31 +6,30 @@
 </head>
 <body>
 <?php
-for ($a=1; $a<=30, $a++;){
-    if ($a=1){
-        echo "on the ".$a."st  of the month specs are available";
+for ($a=1; $a<=30, $a++;) {
+    if ($a = 1) {
+        echo "on the " . $a . "st  of the month specs are available";
     }
-    if($a%2==0){
-       if($a=2||$a=22){
-           echo "on the ".$a."nd  of the month specs are available";
-       }
-        else{echo "on the ".$a."th of the month specs are available";}
+    if ($a % 2 == 0) {
+        if ($a = 2 || $a = 22) {
+            echo "on the " . $a . "nd  of the month specs are available";
+        } else {
+            echo "on the " . $a . "th of the month specs are available";
+        }
     }
-   if ($a%3==0){
-       if($a=3){
-           echo "on the ".$a."rd  of the month specs are available";
-       }
-       elseif($a=21){
-           echo "on the ".$a."st  of the month specs are available";
-       } else {echo "on the ".$a."th of the month specs are available";}
+    if ($a % 3 == 0) {
+        if ($a = 3) {
+            echo "on the " . $a . "rd  of the month specs are available";
+        } elseif ($a = 21) {
+            echo "on the " . $a . "st  of the month specs are available";
+        } else {
+            echo "on the " . $a . "th of the month specs are available";
+        }
 
-   }
-    if ($a%4==0){
-        echo  "on the ".$a."th of the month specs are available";
     }
-
-
-
+    if ($a % 4 == 0) {
+        echo "on the " . $a . "th of the month specs are available";
+    }
 }
 ?>
 
