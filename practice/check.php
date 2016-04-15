1db64530e9953b88f7b50bdc7f2f32c08d9808b4
1509184 <fm.oyindamola@gmail.com>
2016-04-15 18:30:23 +0100

    week9b

---
+++ practice/check.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>check</title>
+</head>
+<body>
+<?php
+$myAge=$_GET['age'];
+$username=$_GET['username'];
+
+$adult="you are allowed to buy mugs or sausage rolls or Specs";
+$middle_age="you are allowed to buy mugs or sausage rolls";
+$teenager="you are allowed to buy only mugs";
+$kid="you are too young";
+if($myAge<=16){
+    echo $kid;
+}
+elseif($myAge>16 && $myAge<=18){
+    echo $teenager;
+}
+elseif($myAge>18 && $myAge<=21){
+    echo $middle_age;
+}
+else{echo $adult;}
+?>
+</body>
+</html>
