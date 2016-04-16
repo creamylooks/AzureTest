be0f09c0bb98e5d3777564d56d1eeff32a0288dc
1509184 <fm.oyindamola@gmail.com>
2016-04-16 11:03:52 +0100

    week9b

--- practice/mugs.php
+++ practice/mugs.php
@@ -11,13 +11,13 @@ for ($i=1; $i<4; $i++) {
     $winner = array_rand($names, 1);
     switch($i){
         case 1:
-            echo"The winner of the smugs is ". $names[$winner]."<br>";
+            echo"The winner of the smugs is ". strtoupper($names[$winner])."<br>";
             break;
         case 2:
-            echo"The winner of the cream is ". $names[$winner]."<br>";
+            echo"The winner of the cream is ". strtoupper($names[$winner])."<br>";
             break;
         case 3:
-            echo"The winner of the sausage rolls is ". $names[$winner]."<br>";
+            echo"The winner of the sausage rolls is ".strtoupper( $names[$winner])."<br>";
             break;
     }
 
