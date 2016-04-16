f3f17c68c0c96f8556dc2b34de8b317b3e185012
1509184 <fm.oyindamola@gmail.com>
2016-04-16 10:59:09 +0100

    week9b

--- practice/mugs.php
+++ practice/mugs.php
@@ -5,19 +5,19 @@
  * Date: 16/04/2016
  * Time: 09:37
  * */
-$names=array("fffd","fdfdsfsd","DVSDV","ffdsaf","GGSGS","DFDSFSD","DGSDg","FSDBFGF","EFEWFew","WGdsgsd","FEDFD");
+$names=array("derin","toke","lanre","idris","admijn","mike","adams","femi","jake","terry","matt");
 sort($names);
 for ($i=1; $i<4; $i++) {
     $winner = array_rand($names, 1);
     switch($i){
         case 1:
-            echo"The winner of the smugs is". $names[$winner]."<br>";
+            echo"The winner of the smugs is ". $names[$winner]."<br>";
             break;
         case 2:
-            echo"The winner of the cream is". $names[$winner]."<br>";
+            echo"The winner of the cream is ". $names[$winner]."<br>";
             break;
         case 3:
-            echo"The winner of the sausage rolls is". $names[$winner]."<br>";
+            echo"The winner of the sausage rolls is ". $names[$winner]."<br>";
             break;
     }
 
