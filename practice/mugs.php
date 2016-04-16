0bcc5f18ba5f3c8ac77c39351bb8f6103f113a43
1509184 <fm.oyindamola@gmail.com>
2016-04-16 09:40:37 +0100

    week9b

---
+++ practice/mugs.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1509184
+ * Date: 16/04/2016
+ * Time: 09:37
+ */
+$provs=array("mugs","specs","rock and roll");
+foreach($provs as $x){
+    echo "<p>$x</x>";
+}
