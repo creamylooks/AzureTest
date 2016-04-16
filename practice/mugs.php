db21346ff5d9d66eb8c0983fb761c3668064cc39
1509184 <fm.oyindamola@gmail.com>
2016-04-16 10:10:15 +0100

    week9b

--- practice/mugs.php
+++ practice/mugs.php
@@ -4,34 +4,30 @@
  * User: 1509184
  * Date: 16/04/2016
  * Time: 09:37
- */
-/*$provs=array("mugs","specs","rock and roll");
-$provs[0]="hugs";
-foreach($provs as $x){
-    echo "<p>$x</x>";
-}
-*/
-$myArray = array("specs", "mugs", "sausage rolls");
-
-echo "<h1>Original Array</h1>";
-printmyArray($myArray);
-
-$myArray[1] = "hugs"; // modifies position 1 (re)
-
-echo "<h1>Swap in Hugs</h1>";
-printmyArray($myArray);
-
-unset($myArray[2]); // removes the array in position 2
-
-echo "<h1>Take out Sausage Rolls</h1>";
-printmyArray($myArray);
-
-
-
-//I've made a function to print out the array instead of writing the same code repeatedly above.
-function printmyArray($myArray)
+ * */
+for ($i = 1; $i < 30; $i++)
 {
-    foreach($myArray as $x) {
-        echo "<p>" . $x . "</p>";
-    }
+$y = 0; //I'm using this as a marker to tell that at least one thing is being made
+echo "<p>On day " . $i . " the following products are available: ";
+
+if ($i % 2 == 0)
+{
+echo "Specs ";
+$y = 1;
+}
+if ($i % 3 == 0)
+{
+echo "Mugs ";
+$y = 1;
+}
+if ($i % 4 == 0)
+{
+echo "Sausage Rolls";
+$y = 1;
+}
+if ($y != 1) //if one thing isn't being made on this day then it displays that nothing is available that day.
+{
+echo "NONE";
+}
+echo "</p>";
 }
