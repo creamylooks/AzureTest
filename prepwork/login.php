04131d102d2c07fa1a67dd16f33764e5bc57bd8d
1509184 <fm.oyindamola@gmail.com>
2016-04-18 15:46:05 +0100

    week9b

---
+++ prepwork/login.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1509184
+ * Date: 18/04/2016
+ * Time: 15:29
+ */
+include('dbconnect.php');
+
+if(empty($_POST['username'])|| empty ($_POST['password'])){
+    echo "Both fields are Required";
+}
+else{
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+
+    $sql= "SELECT p_id FROM people WHERE username=$username AND password=$password";
+    $result=mysqli_query($db,$sql);
+    if(mysqli_num_rows($result)==1){
+        header("location:home.php");
+    }else{echo "Username or password incorrect.";}
+}
