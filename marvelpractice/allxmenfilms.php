61163f037a23cae4c12dc9bf8478748af64ea907
1509184 <fm.oyindamola@gmail.com>
2016-04-18 19:13:38 +0100

    week9b

---
+++ marvelpractice/allxmenfilms.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1509184
+ * Date: 18/04/2016
+ * Time: 18:02
+ */
+$sql=mysqli_query("SELECT * FROM marvelmovies WHERE title LIKE 'X-Men'");
+$result=mysql_query ($db,$sql);
+echo "<p>"."Movie ID"."</p>". "&nbsp;&nbsp;&nbsp;"."<p>"."Year Released"."</p>". "&nbsp;&nbsp;&nbsp;"."<p>"."Title"."</p>"."&nbsp;&nbsp;&nbsp;"."<p>"."Production Studio"."</p>"."&nbsp;&nbsp;&nbsp;"."<p>"."Notes"."</p>"."<br><br>";
+while($row = $result->fetch_array()){
+    $id=$row['marvelMovieID'];
+    $movietitle=$row['title'];
+    $movieyear=$row['yearReleased'];
+    $studio=$row['Production Studio'];
+    $comments=$row['notes'];
+    echo "<p>".$id."</p>". "&nbsp;&nbsp;&nbsp;"."<p>".$movieyear."</p>". "&nbsp;&nbsp;&nbsp;"."<p>".$movietitle."</p>"."&nbsp;&nbsp;&nbsp;"."<p>".$studio."</p>"."&nbsp;&nbsp;&nbsp;"."<p>".$comments."</p>"."<br><br>";
+}
---
+++ marvelpractice/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+ <html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login Page</title>
+    </head>
+    <body>
+        <div id="loginbox">
+            <form action="moviesDisplay.php" method="post">
+                <label>Username:</label><br>
+                <input type="text" name="username" placeholder="user"><br><br>
+                <label>Password:</label>
+                <input type="password" name="password" placeholder="password">
+                <button>Submit</button>
+            </form>
+        </div>
+    </body>
+ </html>
