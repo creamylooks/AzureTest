a207a0ae244f0b715d0599e29b2f38cb1e9dad47
1509184 <fm.oyindamola@gmail.com>
2016-04-19 09:59:53 +0100

    week9b

--- marvelpractice/home.php
+++ marvelpractice/home.php
@@ -5,7 +5,7 @@
         <title>Welcome</title>
     </head>
     <body>
-    <form action="Movies.php" method="get">
+    <form action="moviesDisplay.php" method="get">
         <button name="All Movies">All Movies</button>
         <button>Movies By Marvel</button>
         <button>Movies After 2010</button>
