0040b52a75532d1ebcf9aab10124493e47ecb9d7
1509184 <fm.oyindamola@gmail.com>
2016-04-19 14:29:57 +0100

    week9b

--- marvelpractice/Movies.php
+++ marvelpractice/Movies.php
@@ -7,7 +7,7 @@
  */
 $resp = $_GET['movies'];
 include('dbconnect.php');
-echo "Movie ID" . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . "Year Released" . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . "Title" . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . "Production Studio" . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . "Notes" . "<br><br>";
+//echo "Movie ID" . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . "Year Released" . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . "Title" . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . "Production Studio" . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . "Notes" . "<br><br>";
 
 switch ($resp) {
     case 'allMovies':
@@ -15,23 +15,33 @@ switch ($resp) {
         $sql = "SELECT * FROM marvelmovies";
         $result = $db->query($sql);
         $row = $result->fetch_array();
-
+        echo "<table>
+            <thead>
+            <tr><td>Id</td><td>Movie ID</td><td>Year Released</td><td>Title</td><td>Production Studio</td><td>Notes</td>
+            </tr></thead>";
         while ( $row = $result->fetch_array()) {
             $id = $row['marvelMovieID'];
             $movietitle = $row['title'];
             $movieyear = $row['yearReleased'];
             $studio = $row['Production Studio'];
             $comments = $row['notes'];
-            echo "{$id} {$movieyear}{$movietitle}{$studio}{$comments}";
+            echo "
+            <td>{$id}</td>
+            <td>{$movieyear}</td>
+            <td>{$movietitle}</td>
+            <td>{$studio}</td>
+            <td>{$comments}</td>
+             ";
 
         }
+        echo "</table>";
         break;
     case 'moviesbymarvel':
         $sql = "SELECT * FROM marvelmovies WHERE productionStudio= 'Marvel Studios'";
         $result = $db->query($sql);
         $row = $result->fetch_array();
 
-        while ($row) {
+        while ($row = $result->fetch_array()) {
             $id = $row['marvelMovieID'];
             $movietitle = $row['title'];
             $movieyear = $row['yearReleased'];
@@ -46,7 +56,7 @@ switch ($resp) {
         $result = $db->query($sql);
         $row = $result->fetch_array();
 
-        while ($row) {
+        while ( $row = $result->fetch_array()) {
             $id = $row['marvelMovieID'];
             $movietitle = $row['title'];
             $movieyear = $row['yearReleased'];
@@ -61,7 +71,7 @@ switch ($resp) {
         $result = $db->query($sql);
         $row = $result->fetch_array();
 
-        while ($row) {
+        while ( $row = $result->fetch_array()) {
             $id = $row['marvelMovieID'];
             $movietitle = $row['title'];
             $movieyear = $row['yearReleased'];
