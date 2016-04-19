84a7edd672d8db39752089773dfe77c8645d79e7
1509184 <fm.oyindamola@gmail.com>
2016-04-19 10:39:29 +0100

    week9b

--- marvelpractice/Movies.php
+++ marvelpractice/Movies.php
@@ -5,22 +5,22 @@
  * Date: 18/04/2016
  * Time: 19:57
  */
-$resp=$_GET['movies'];
+$resp = $_GET['movies'];
 include('dbconnect.php');
-echo "Movie ID"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."Year Released"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."Title"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."Production Studio"."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."Notes"."<br><br>";
+echo "Movie ID" . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . "Year Released" . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . "Title" . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . "Production Studio" . "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . "Notes" . "<br><br>";
 
-switch($resp){
+switch ($resp) {
     case 'allMovies':
-        $sql=mysqli_query("SELECT * FROM marvelmovies");
-        $result=mysql_query ($db,$sql);
+        $sql = mysqli_query("SELECT * FROM marvelmovies");
+        $result = mysql_query($db, $sql);
         $row = $result->fetch_array();
-        $id = $row['marvelMovieID'];
-        $movietitle = $row['title'];
-        $movieyear = $row['yearReleased'];
-        $studio = $row['Production Studio'];
-        $comments = $row['notes'];
 
-        while($row) {
+        while ($row = $result->fetch_array()) {
+            $id = $row['marvelMovieID'];
+            $movietitle = $row['title'];
+            $movieyear = $row['yearReleased'];
+            $studio = $row['Production Studio'];
+            $comments = $row['notes'];
 
             echo "<p>{$id}</p>
             <p>{$movieyear}</p>
@@ -29,23 +29,23 @@ switch($resp){
             <p>{$comments}</p>";
 
         }
-    break;
+        break;
     case $marvel:
-        $sql=mysqli_query("SELECT * FROM marvelmovies WHERE productionStudio= 'Marvel Studios'");
-        while($row){
-            echo "<p>".$id."</p>". "&nbsp;&nbsp;&nbsp;"."<p>".$movieyear."</p>". "&nbsp;&nbsp;&nbsp;"."<p>".$movietitle."</p>"."&nbsp;&nbsp;&nbsp;"."<p>".$studio."</p>"."&nbsp;&nbsp;&nbsp;"."<p>".$comments."</p>"."<br><br>";
+        $sql = mysqli_query("SELECT * FROM marvelmovies WHERE productionStudio= 'Marvel Studios'");
+        while ($row) {
+            echo "<p>" . $id . "</p>" . "&nbsp;&nbsp;&nbsp;" . "<p>" . $movieyear . "</p>" . "&nbsp;&nbsp;&nbsp;" . "<p>" . $movietitle . "</p>" . "&nbsp;&nbsp;&nbsp;" . "<p>" . $studio . "</p>" . "&nbsp;&nbsp;&nbsp;" . "<p>" . $comments . "</p>" . "<br><br>";
         }
-    break;
+        break;
     case $byYear:
-        $sql=mysqli_query("SELECT * FROM marvelmovies WHERE yearReleased>='2010'");
-        while($row){
-            echo "<p>".$id."</p>". "&nbsp;&nbsp;&nbsp;"."<p>".$movieyear."</p>". "&nbsp;&nbsp;&nbsp;"."<p>".$movietitle."</p>"."&nbsp;&nbsp;&nbsp;"."<p>".$studio."</p>"."&nbsp;&nbsp;&nbsp;"."<p>".$comments."</p>"."<br><br>";
+        $sql = mysqli_query("SELECT * FROM marvelmovies WHERE yearReleased>='2010'");
+        while ($row) {
+            echo "<p>" . $id . "</p>" . "&nbsp;&nbsp;&nbsp;" . "<p>" . $movieyear . "</p>" . "&nbsp;&nbsp;&nbsp;" . "<p>" . $movietitle . "</p>" . "&nbsp;&nbsp;&nbsp;" . "<p>" . $studio . "</p>" . "&nbsp;&nbsp;&nbsp;" . "<p>" . $comments . "</p>" . "<br><br>";
         }
-    break;
+        break;
     case $xMen:
-        $sql=mysqli_query("SELECT * FROM marvelmovies WHERE title LIKE 'X-Men'");
-        while($row){
-            echo "<p>".$id."</p>". "&nbsp;&nbsp;&nbsp;"."<p>".$movieyear."</p>". "&nbsp;&nbsp;&nbsp;"."<p>".$movietitle."</p>"."&nbsp;&nbsp;&nbsp;"."<p>".$studio."</p>"."&nbsp;&nbsp;&nbsp;"."<p>".$comments."</p>"."<br><br>";
+        $sql = mysqli_query("SELECT * FROM marvelmovies WHERE title LIKE 'X-Men'");
+        while ($row) {
+            echo "<p>" . $id . "</p>" . "&nbsp;&nbsp;&nbsp;" . "<p>" . $movieyear . "</p>" . "&nbsp;&nbsp;&nbsp;" . "<p>" . $movietitle . "</p>" . "&nbsp;&nbsp;&nbsp;" . "<p>" . $studio . "</p>" . "&nbsp;&nbsp;&nbsp;" . "<p>" . $comments . "</p>" . "<br><br>";
         }
 
 }
