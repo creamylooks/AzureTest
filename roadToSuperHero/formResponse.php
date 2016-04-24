5a50a7623a7b559ba8d41e15edab7801aa8466bc
1509184 <fm.oyindamola@gmail.com>
2016-04-24 16:26:16 +0100

    week9b

---
+++ roadToSuperHero/formResponse.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1509184
+ * Date: 24/04/2016
+ * Time: 16:02
+ */
+
+$name1=$_POST['fname'];
+$name2=$_POST['lname'];
+$gender=$_POST['sex'];
+echo "$name1 $name2 $gender";
