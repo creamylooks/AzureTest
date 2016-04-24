72fe5a07555c3bbf164b4ec963fc025910af722e
1509184 <fm.oyindamola@gmail.com>
2016-04-24 15:15:51 +0100

    week9b

--- MarvelRepeat/xMenMovies.php
+++ MarvelRepeat/xMenMovies.php
@@ -5,3 +5,17 @@
  * Date: 24/04/2016
  * Time: 13:24
  */
+include ('connection.php');
+$collect= "SELECT * FROM marvelmovies WHERE title LIKE 'X-MEN'";
+$pointer= mysqli_query($sercon,$collect);
+//or      $pointer=$sercon->query($collect);
+$row=(mysqli_num_rows($pointer));
+while($row>0){
+    $result=mysqli_fetch_array($pointer);
+    echo "
+          {$result['yearReleased']};
+          {$result['title']};
+          {$result['productionStudio']};
+          {$result['notes']} ";
+    $row--;
+}
