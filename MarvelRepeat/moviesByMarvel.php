fe5e5302252b7e4e9af12bde4a47f75c32d04138
1509184 <fm.oyindamola@gmail.com>
2016-04-24 15:10:19 +0100

    week9b

--- MarvelRepeat/moviesByMarvel.php
+++ MarvelRepeat/moviesByMarvel.php
@@ -5,4 +5,17 @@
  * Date: 24/04/2016
  * Time: 13:23
  */
-
+include ('connection.php');
+$collect= "SELECT * FROM marvelmovies WHERE productionStudio= ' Marvel Studios'";
+$pointer= mysqli_query($sercon,$collect);
+//or      $pointer=$sercon->query($collect);
+$row=(mysqli_num_rows($pointer));
+while($row>0){
+    $result=mysqli_fetch_array($pointer);
+    echo "
+          {$result['yearReleased']};
+          {$result['title']};
+          {$result['productionStudio']};
+          {$result['notes']} ";
+    $row--;
+}
