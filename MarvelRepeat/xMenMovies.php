d8d58e7c479cf29692b52746bbee35c064c087b7
1509184 <fm.oyindamola@gmail.com>
2016-04-24 15:16:48 +0100

    week9b

--- MarvelRepeat/xMenMovies.php
+++ MarvelRepeat/xMenMovies.php
@@ -6,7 +6,7 @@
  * Time: 13:24
  */
 include ('connection.php');
-$collect= "SELECT * FROM marvelmovies WHERE title LIKE 'X-MEN'";
+$collect= "SELECT * FROM marvelmovies WHERE title LIKE '%X-MEN%'";
 $pointer= mysqli_query($sercon,$collect);
 //or      $pointer=$sercon->query($collect);
 $row=(mysqli_num_rows($pointer));
