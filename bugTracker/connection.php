8958e2cbb83887447c01ec5a257eb86758563cfc
1509184 <fm.oyindamola@gmail.com>
2016-05-03 18:36:17 +0100

    just commiting

--- bugTracker/connection.php
+++ bugTracker/connection.php
@@ -11,10 +11,10 @@ define ('PASSWORD','12d5f6d8');
 define ('HOST','creamy');
 $sercon=mysqli_connect (SERVER,USER,PASSWORD,HOST);
 
-if ($sercon->connect_error) {
-    die("Connection failed: " . $sercon->connect_error);
-}
-echo "Connected successfully";
+//if ($sercon->connect_error) {
+    //die("Connection failed: " . $sercon->connect_error);
+//}
+//echo "Connected successfully";
 
 
 
