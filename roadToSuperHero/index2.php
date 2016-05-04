4b80122f147f446204d7caea363007bfc5ebc435
creamylooks <f.o.malick@rgu.ac.uk>
2016-05-04 02:23:58 +0100

    Update index2.php

--- roadToSuperHero/index2.php
+++ roadToSuperHero/index2.php
@@ -9,7 +9,7 @@
         <h1>Welcome to the world of Super heroes</h1>
         <h3>What would you like to do?</h3>
         <ul>
-            <li><a href="superheroform.php">Insert a Superhero</a></li>
+            <li><a href="superheroform.html">Insert a Superhero</a></li>
             <li><a href="Showsuperheros.php">Display all Superheroes</a></li>
             <li><a href="Insertbattle.php">Insert a Battle</a></li>
             <li><a href="displaybattles.php">Display all Battles</a></li>
