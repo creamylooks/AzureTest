b1d3c40b710b0101023c76418f2288a564c4562c
Malick <fm.oyindamola@gmail.com>
2016-05-25 17:13:45 +0100

    typharh's website

---
+++ typharh/home.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+            <header>
+                        <img src="#" alt="brand logo"/>
+                        <div>
+                            <img src="#" alt="search icon"/>
+                            <p>Search Website</p>
+                            <p>Go</p>
+                        </div>
+                        <a href="#"><p>Sign in</p></a>
+                        <a href="#"><p>Register</p></a>
+            </header>
+            <nav>
+                    <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">PRODUCTS</a></li>
+                            <li><a href="#">ABOUT US</a></li>
+                            <li><a href="#">TESTIMONIALS</a></li>
+                            <li><a href="#">CONTACT US</a></li>
+                    </ul>
+            </nav>
+            <main>
+                    <section>
+                        <img src="#" alt="main image"/>
+                        <div>
+                            <h3>New Arrivals</h3>
+                            <div class="individual">
+                                <img src="#" alt="image 1"/>
+                                <p>Put its price here</p>
+                            </div>
+                            <div class="individual">
+                            <img src="#" alt="image 2"/>
+                            <p>Put its price here</p>
+                            </div>
+                            <div class="individual">
+                            <img src="#" alt="image 3"/>
+                            <p>Put its price here</p>
+                            </div>
+                            <div class="individual">
+                            <img src="#" alt="image 4"/>
+                            <p>Put its price here</p>
+                            </div>
+                            <div class="individual">
+                            <img src="#" alt="image 5"/>
+                            <p>Put its price here</p>
+                            </div>
+                            <div class="individual">
+                            <img src="#" alt="image 6"/>
+                            <p>Put its price here</p>
+                            </div>
+                        </div>
+                    </section>
+                    <aside>
+                            <div>
+
+                            </div>
+                            <div>
+
+                            </div>
+                    </aside>
+
+            </main>
+            <footer>
+                <h3></h3>
+                <img src="#" alt="logo"/>
+                <img src="#" alt="visa logo"/>
+                <img src="#" alt="mastercard logo"/>
+                <img src="#" alt="paypal logo"/>
+                <img src="#" alt="facebook logo"/>
+                <img src="#" alt="twitter logo"/>
+                <img src="#" alt="instagram logo"/>
+                <a href="#"><img src="#" alt="back to top"></a>
+                <p>copyright bullshit</p>
+            </footer>
+</body>
+</html>
