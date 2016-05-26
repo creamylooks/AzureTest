511ed561924aa65e9e060cb07721e839a06d285a
Malick <fm.oyindamola@gmail.com>
2016-05-26 20:17:50 +0100

    typharh's website

--- typharh/login.php
+++ typharh/login.php
@@ -1,7 +1,28 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Malick
- * Date: 26/05/2016
- * Time: 12:40
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About Us</title>
+</head>
+<body>
+<header>
+    <img src="Typhar.png" alt="brand logo"/>
+    <div>
+        <img src="searchicon.png" alt="search icon"/>
+        <p>Search Website</p>
+        <p>Go</p>
+    </div>
+    <a href="login.php"><p>Sign in</p></a>
+    <a href="signup.php"><p>Register</p></a>
+</header>
+<nav>
+    <ul>
+        <li><a href="home.php">HOME</a></li>
+        <li><a href="products.php">PRODUCTS</a></li>
+        <li><a href="about.php">ABOUT US</a></li>
+        <li><a href="testimonials.php">TESTIMONIALS</a></li>
+        <li><a href="contact.php">CONTACT US</a></li>
+    </ul>
+</nav>
+</body>
+</html>
