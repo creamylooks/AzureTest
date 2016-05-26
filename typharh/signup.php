66dfb8a0c23cff95ac6c5226b54b13d50db8c377
Malick <fm.oyindamola@gmail.com>
2016-05-26 20:26:53 +0100

    typharh's website

--- typharh/signup.php
+++ typharh/signup.php
@@ -5,24 +5,51 @@
     <title>About Us</title>
 </head>
 <body>
-<header>
-    <img src="Typhar.png" alt="brand logo"/>
-    <div>
-        <img src="searchicon.png" alt="search icon"/>
-        <p>Search Website</p>
-        <p>Go</p>
-    </div>
-    <a href="login.php"><p>Sign in</p></a>
-    <a href="signup.php"><p>Register</p></a>
-</header>
-<nav>
-    <ul>
-        <li><a href="home.php">HOME</a></li>
-        <li><a href="products.php">PRODUCTS</a></li>
-        <li><a href="about.php">ABOUT US</a></li>
-        <li><a href="testimonials.php">TESTIMONIALS</a></li>
-        <li><a href="contact.php">CONTACT US</a></li>
-    </ul>
-</nav>
+            <header>
+                <img src="Typhar.png" alt="brand logo"/>
+                <div>
+                    <img src="searchicon.png" alt="search icon"/>
+                    <p>Search Website</p>
+                    <p>Go</p>
+                </div>
+                <a href="login.php"><p>Sign in</p></a>
+                <a href="signup.php"><p>Register</p></a>
+            </header>
+            <nav>
+                <ul>
+                    <li><a href="home.php">HOME</a></li>
+                    <li><a href="products.php">PRODUCTS</a></li>
+                    <li><a href="about.php">ABOUT US</a></li>
+                    <li><a href="testimonials.php">TESTIMONIALS</a></li>
+                    <li><a href="contact.php">CONTACT US</a></li>
+                </ul>
+            </nav>
+            <main>
+                <section>
+            
+                </section>
+                <aside>
+            
+                </aside>
+            </main>
+            <footer>
+                <h3>Connect With Us</h3>
+                <div>
+                    <img src="Typhar.png" alt="brand logo"/>
+                    <img src="visa_logo.jpg" alt="visa logo"/>
+                    <img src="MasterCard.jpg" alt="mastercard logo"/>
+                    <img src="Paypal_logo.png" alt="paypal logo"/>
+                </div>
+                <div>
+                    <a href="https://www.facebook.com/profile.php?id=100007788017332"><img src="fb.jpg" alt="facebook logo"/></a>
+                    <a href="https://twitter.com/Hacktypharh"><img src="twitter.png" alt="twitter logo"/></a>
+                    <a href="https://www.instagram.com/alluring_typharh"><img src="instagram.png" alt="instagram logo"/></a>
+                </div>
+                <div>
+                    <a href="https://www.creamy.azurewebsites.net/typharh/home.php"><img src="#" alt="back to top arrow"></a>
+                    <p>Back to Top</p>
+                </div>
+                <p>Copyright &copy; Alluring Typharh 2016</p>
+            </footer>
 </body>
 </html>
